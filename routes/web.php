0c8d0302c8f931a11ee7f40e6b32cdab17653ddf
Miiso9 <mihaelmiso555@gmail.com>
2023-02-04 16:45:37 +0100

    comm

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-URL::forceRootUrl('http://studenti.sum.ba/projekti/rwa/2022/g3');
+//URL::forceRootUrl('http://studenti.sum.ba/projekti/rwa/2022/g3');
 
 Route::get('/', [WelcomeController::class, 'index']);
 Route::get('/events', [FrontendEventController::class,'index'])->name('events.index');
